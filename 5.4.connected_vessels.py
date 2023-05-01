01b5b8d6078f6946cc6de307fcdc0cfedd4b70c0
adiel dror <adiel.dror2@gmail.com>
2023-05-02 00:34:42 +0300

    for changing the loops

--- 5.4.connected_vessels.py
+++ 5.4.connected_vessels.py
@@ -2,36 +2,20 @@
 def interleave(*iterable):
     if not iterable:
         return None
-    res = []
-    j = 0
 
-    # If we haven't reached the end of the first element
-    # - assuming that the size of all elements is the same
-    while j < len(iterable[0]):
-        for i in iterable:
-            if type(i) is dict:  # Checking if the element is a dictionary
-                res += [list(i)[j]]
-
-            else:
-                res += [i[j]]
-        j += 1
-    return res
+    return [list(inner_loop.keys())[outer_loop] if type(inner_loop) is dict else inner_loop[outer_loop] for outer_loop
+            in range(len(iterable[0])) for inner_loop in iterable]
 
 
 # Version with generator
 def interleave_generator(*iterable):
-    j = 0
-    while j < len(iterable[0]):
-        for i in iterable:
-            if type(i) is dict:
-                yield list(i)[j]
-            else:
-                yield i[j]
-        j += 1
+    return (list(inner_loop.keys())[outer_loop] if type(inner_loop) is dict else inner_loop[outer_loop] for outer_loop
+            in range(len(iterable[0])) for inner_loop in iterable)
 
 
-print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
-print(interleave('abc', [1, 2, 3], ('!', '@', '#'), {6: 'd', 7: 'f', 8: 'g'}))
-print(interleave())
-for element in interleave_generator('abc', [1, 2, 3], ('!', '@', '#'), {6: 'd', 7: 'f', 8: 'g'}):
-    print(element)
+if __name__ == '__main__':
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#'), {6: 'd', 7: 'f', 8: 'g'}))
+    print(interleave())
+    for element in interleave_generator('abc', [1, 2, 3], ('!', '@', '#'), {6: 'd', 7: 'f', 8: 'g'}):
+        print(element)
